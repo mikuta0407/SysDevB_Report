5a9ca4ff27be0568609b874ec3757e3485dd924d
mikuta0407 <flute.takkun0407@gmail.com>
2019-12-13 16:13:18 +0900

    全部上げ

---
+++ Test02/app/src/main/java/com/mikuta0407/test02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mikuta0407.test02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
