9e18d40969c70f4c718f80463fe17d2e97604e0a
mikuta0407 <flute.takkun0407@gmail.com>
2019-12-17 11:29:06 +0900

    レイアウトの再構成

--- MyRepo04A/app/src/main/java/com/mikuta0407/myrepo04a/MainActivity.java
+++ MyRepo04A/app/src/main/java/com/mikuta0407/myrepo04a/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static com.mikuta0407.myrepo04a.R.id.disp_time;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView disp_time = findViewById(R.id.disp_time);
     }
 }
