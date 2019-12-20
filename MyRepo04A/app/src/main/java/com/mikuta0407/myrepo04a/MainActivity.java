b955d8e224f3888312298ecba7cc150328162c2b
mikuta0407 <mikuta0407@hs-sh.net>
2019-12-20 23:08:18 +0900

    回転処理はできた

--- MyRepo04A/app/src/main/java/com/mikuta0407/myrepo04a/MainActivity.java
+++ MyRepo04A/app/src/main/java/com/mikuta0407/myrepo04a/MainActivity.java
@@ -22,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     boolean run = false; //タイマーが動作中かそうじゃないかを記録
     boolean finished = true; //一時停止状態か、終了/キャンセルで止まったのかを記録。
+    boolean pfinished = false; //発表時間が終わったかどうか
     boolean question = false; //質問タイムかどうかの判定
 
     private TextView timerText; //数字表示部のTextView
     private ProgressBar timeProgressBar; //プログレスバー
+    private FloatingActionButton start_pause;
+    private FloatingActionButton cancel;
     private CountDownTimer countDown; //カウントダウンクラス
     private SimpleDateFormat dataFormat = new SimpleDateFormat("mm:ss", java.util.Locale.JAPANESE); //データフォーマット
 
@@ -40,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // いろいろ定義たいむ
-        final FloatingActionButton start_pause = findViewById(R.id.start_pause);
-        final FloatingActionButton cancel = findViewById(R.id.cancel);
+        start_pause = findViewById(R.id.start_pause);
+        cancel = findViewById(R.id.cancel);
         timerText = findViewById(R.id.disp_time);
         timerText.setText(dataFormat.format(0));
         timeProgressBar = findViewById(R.id.progressBar);
@@ -51,27 +54,36 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState!=null) {
             // まず動作状態を復元
             run = savedInstanceState.getBoolean("runstatus");
+            finished = savedInstanceState.getBoolean("finishedstatus");
+            question = savedInstanceState.getBoolean("questionstatus");
+            pfinished = savedInstanceState.getBoolean("pfinishedstatus");
 
-            
+            //数値系
+            ptime = savedInstanceState.getLong("ptimestatus");
+            qtime = savedInstanceState.getLong("qtimestatus");
+            leftTime = savedInstanceState.getLong("leftTimestatus");
+
             // スタート・ストップボタンの画像状態を復元
             if (run) { // 動作中なら
                 start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause)); //一時停止ボタンに変更.
+                startTimer(); //タイマー続行
             } else {   // 動いてなかったら
                 start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_play)); //再生ボタンに変更
             }
+
         }
 
+
+        // 以下メイン処理
 
         // スタート・ストップボタンが押されたら
         start_pause.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View spClick) {
                 if (run) { //動作中なら
-                    run = false; //動作をオフにして
                     start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_play)); //再生ボタンに変更
                     pauseTimer();
                 } else {        // 動いてなかったら
-                    run = true; // オンにして
                     start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause)); //一時停止ボタンに変更
                     if (finished == true){
                         // 発表時間設定の選択を取得
@@ -94,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                         // qtimeに時間設定。(ms)
                         if (qtimeId == R.id.qtime10) {
                             //qtime = 300000;  //5*60*1000 ms
-                            qtime = 10000;
+                            qtime = 15000;
                         } else if (qtimeId == R.id.qtime10) {
                             qtime = 600000;  //10*60*1000 ms
                         }
@@ -115,16 +127,21 @@ public class MainActivity extends AppCompatActivity {
                 run = false;
                 finished = true;
                 question = false;
+                pfinished = false;
             }
         });
     }
 
 
     private void startTimer() {  // タイマー実行
-        if (question == false) {
-            leftTime = ptime;
-        } else {
-            leftTime = qtime;
+        if ((pfinished == false) && (question == false)){ //まだ発表時間が終わってなくて、
+            if (!run) { //動作中じゃなかったら(回転後じゃなくて純粋なオンだったら)
+                run = true;
+                leftTime = ptime;
+            }
+        } else if ((pfinished == true) && (question == false)){ // 発表が終わってて回転後じゃなくてonFinishから呼ばれたら
+                question = true;
+                leftTime = qtime;
         }
             countDown = new CountDownTimer(leftTime,1000) {
             @Override
@@ -134,17 +151,18 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onFinish() {
-                if (question == false) {
-                    question = true;
-                } else {
-                    run = false;
-                    finished = true;
-                    question = false;
+                if (question == false) { //まだ質問に入ってなかったら(発表が終わったら)
+                    pfinished = true; //発表は完了
+                    startTimer(); //質問時間スタート
+                } else { //質問も終わったら
+                    run = false; //動作じゃないよ
+                    finished = true; //おわったよ
+                    question = false; //質問も終わったよ
+                    pfinished = false;  //状態戻すよ
+                    start_pause.setImageDrawable(getResources().getDrawable(R.drawable.ic_play)); //再生ボタンに変更
                 }
             }
         }.start();
-
-        run = true; //スタートしたので
 
     }
 
@@ -181,11 +199,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //メンバ変数runを保存
+        //boolarn系
         outState.putBoolean("runstatus", run);
-        //ptimeとqtime
+        outState.putBoolean("finishedstatus", finished);
+        outState.putBoolean("questionstatus", question);
+        outState.putBoolean("pfinishedstatus", pfinished);
+
+        //数値系
         outState.putLong("ptimestatus", ptime);
         outState.putLong("qtimestatus", qtime);
+        outState.putLong("leftTimestatus", leftTime);
     }
 
 }
