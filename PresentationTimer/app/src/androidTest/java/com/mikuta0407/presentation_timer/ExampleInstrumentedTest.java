86e7e43725679cc516c81bdf666210bb041b3bf5
mikuta0407 <flute.takkun0407@gmail.com>
2020-01-17 14:51:11 +0900

    プロジェクト名編集

--- PresentationTimer/app/src/androidTest/java/com/mikuta0407/presentation_timer/ExampleInstrumentedTest.java
+++ PresentationTimer/app/src/androidTest/java/com/mikuta0407/presentation_timer/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.mikuta0407.myrepo04a", appContext.getPackageName());
+        assertEquals("com.mikuta0407.presentation_timer", appContext.getPackageName());
     }
 }
